3e97b10657afe8683536522ce0896296fe7f65d2
VInit Patel <vinit1411@gmail.com>
2024-05-16 18:01:37 +0530

    Add User Edit Profile Page
    Add User Edit Profile Page and Created the design for it

--- src/app/components/user/user-edit/user-edit.component.ts
+++ src/app/components/user/user-edit/user-edit.component.ts
@@ -2,15 +2,27 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../header/header.component";
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-
+import { CurrentUserDTO } from '../../../models/user-models';
+import { UserService } from '../../../services/user.service';
+import {MatListModule} from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
     selector: 'app-user-edit',
     standalone: true,
     templateUrl: './user-edit.component.html',
     styleUrl: './user-edit.component.css',
-    imports: [HeaderComponent,MatCardModule, MatButtonModule]
+    imports: [HeaderComponent,MatCardModule, MatButtonModule, MatDividerModule,MatFormFieldModule, MatInputModule,
+      MatSelectModule,MatListModule]
 })
-export class UserEditComponent {
 
+export class UserEditComponent {
+  userData: CurrentUserDTO;
+
+  constructor(private userService: UserService){
+    this.userData = userService.currentUserValue();
+  }
 }
